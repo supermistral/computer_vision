1cb6370413ad76909cf5d7aa4a082dbd006aa934
SuperMistral <mediamart37@gmail.com>
2019-12-09 17:34:43 +0300

    Стартовая функция, вывод ролика

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+from cv2 import cv2
 import numpy as np
-import cv2
 
 class Computer_vision:
     def __init__(self):
         self.video = cv2.VideoCapture('video_cv.mp4')
 
-        if self.video.isOpened == False:
+        if not self.video.isOpened:
             print('Error in file')
             return
 
-        video_width = 360
-        video_height = 200
+        self.video_width = 360
+        self.video_height = 200
+        self.start_handler()
+
+    def start_handler(self):
+        while cv2.waitKey(1) != 27:
+            condition, frame = self.video.read()
+            if not condition:
+                print("End")
+                break
+            resized = cv2.resize(frame, (self.video_width, self.video_height))
+            cv2.imshow("videooo", resized)
+
+a = Computer_vision()
